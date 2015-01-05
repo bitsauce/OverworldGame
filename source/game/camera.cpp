5b89c3df63e898cb3e7916fc22a340555c903a5f
bitsauce <marcus.loo.vergara@gmail.com>
2015-01-05 15:54:25 +0100

    Active chunk area

--- source/game/camera.cpp
+++ source/game/camera.cpp
@@ -9,9 +9,17 @@ Camera::Camera() :
 	XInput::addMouseListener(this);
 }
 
-Vector2 Camera::getCenter()
+Vector2 Camera::getCenter() const
 {
 	return m_position + XWindow::getSize() * 0.5f / m_zoom;
+}
+
+Matrix4 Camera::getProjectionMatrix() const
+{
+	Matrix4 mat;
+	mat.translate(-m_position.x, -m_position.y, 0.0f);
+	mat.scale(m_zoom);
+	return mat;
 }
 
 void Camera::lookAt(Vector2 point)
@@ -20,12 +28,9 @@ void Camera::lookAt(Vector2 point)
 	m_position = point;
 }
 
-Matrix4 Camera::getProjectionMatrix()
+Recti Camera::getActiveChunkArea() const
 {
-	Matrix4 mat;
-	mat.translate(-m_position.x, -m_position.y, 0.0f);
-	mat.scale(m_zoom);
-	return mat;
+	return m_activeChunkArea;
 }
 
 Vector2 Camera::getPosition()
@@ -58,33 +63,51 @@ float Camera::getHeight()
 	return getSize().y;
 }
 
+float Camera::getZoom() const
+{
+	return m_zoom;
+}
+
 void Camera::update()
 {
 	if(XInput::getKeyState(XD_KEY_LEFT)) {
 		m_position.x -= 16.0f/m_zoom;
 	}
+	
 	if(XInput::getKeyState(XD_KEY_RIGHT)) {
 		m_position.x += 16.0f/m_zoom;
 	}
+	
 	if(XInput::getKeyState(XD_KEY_UP)) {
 		m_position.y -= 16.0f/m_zoom;
 	}
+	
 	if(XInput::getKeyState(XD_KEY_DOWN)) {
 		m_position.y += 16.0f/m_zoom;
 	}
+	
 	if(XInput::getKeyState(XD_KEY_PLUS)) {
 		Vector2 center = getCenter();
 		m_zoom *= 2.0f;
 		lookAt(center);
 	}
+
 	if(XInput::getKeyState(XD_KEY_MINUS)) {
 		Vector2 center = getCenter();
 		m_zoom *= 0.5f;
 		lookAt(center);
 	}
+	
+	// Update active chunk area
+	m_activeChunkArea.set(
+		(int)floor(m_position.x/CHUNK_PXF), // x
+		(int)floor(m_position.y/CHUNK_PXF), // y
+		(int)floor(m_size.x/CHUNK_PXF),     // width
+		(int)floor(m_size.y/CHUNK_PXF)      // height
+		);
 }
 
-void Camera::draw(xd::SpriteBatch *SpriteBatch)
+void Camera::draw(xd::SpriteBatch*)
 {
 	Vector2 center = getCenter();
 	World::getDebug()->setVariable("Camera", util::floatToStr(center.x) + ", " + util::floatToStr(center.y));
